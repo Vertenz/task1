fe8981a14bfb4bf503372789d39813df1d8f6766
Vertenz <you@example.com>
2021-09-01 01:56:16 +0300

    Test from WS

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '../src/index.html'
+import './css/main.scss'
+import '../src/js/main'
---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dayBlock = document.getElementById('day');
+    const hourBlock = document.getElementById('hour');
+    const minuteBlock = document.getElementById('minutes');
+    const secondBlock = document.getElementById('second');
+
+    let time = null;
+
+    let dayEnd = new Date();
+    dayEnd = +dayEnd + 8.64e7;
+    if (time === null) {
+        setInterval(() => {
+            time = new Date();
+            const timeDifference = dayEnd - time;
+            let dayLeft = timeDifference / 8.64e7;
+            dayLeft = Math.floor(dayLeft);
+            let hourLeft = timeDifference / 3.6e6 % 24;
+            hourLeft = Math.floor(hourLeft);
+            let minutesLeft = timeDifference / 60000 % 60;
+            minutesLeft = Math.floor(minutesLeft);
+            let secondLeft = timeDifference / 1000 % 60;
+            secondLeft = Math.floor(secondLeft)
+            dayBlock.innerHTML = dayLeft;
+            hourBlock.innerHTML = hourLeft;
+            minuteBlock.innerHTML = minutesLeft;
+            secondBlock.innerHTML = secondLeft;
+        }, 1000)
+    }else {
+        //TODO только сек и мин
+    }
+});
